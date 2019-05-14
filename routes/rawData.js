e0a3043b4c36ddbb06e08d421db7234e91656aac
전진호 <gino9940@gmail.com>
2019-05-14 21:50:44 +0900

    Update redirect

--- routes/rawData.js
+++ routes/rawData.js
@@ -3,7 +3,8 @@ var router = express.Router()
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
-	console.log("TCL: req", req.body)
+    console.log("TCL: req", req.body)
+    return res.redirect('/')
 })
 
 module.exports = router
