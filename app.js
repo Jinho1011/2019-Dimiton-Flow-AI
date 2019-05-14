f02b7cbb8ead68f1c00a2317ad68698c3708250d
전진호 <gino9940@gmail.com>
2019-05-14 21:48:34 +0900

    Update logger

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use(logger('dev'))
+app.use(logger({
+  format: 'dev',
+  stream: fs.createWriteStream('app.log', {'flags': 'w'})
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
