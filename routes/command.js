7bcf15f4f686ed9489df4d69624b88c19d6020f3
전진호 <gino9940@gmail.com>
2019-05-17 11:36:54 +0900

    tcp server

--- routes/command.js
+++ routes/command.js
@@ -1,12 +1,39 @@
 var express = require("express");
 var router = express.Router();
+var admin = require("firebase-admin");
+var serviceAccount = require("./automatic-dam-firebase-adminsdk-boz33-fe04b539aa.json");
 
 router.get('/', function (req, res, next) {
   res.render('command')
 })
 
 router.post('/', function(req, res, next) {
-    
+    console.log("TCL: req", req.body)
+    res.redirect('/cmd')
 })
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://automatic-dam.firebaseio.com"
+  });
+
+  // var database = admin.database();
+  // var ref = database.ref("queue/");
+  // ref.push().set('on1')
+
+  // addInQueue.use(cors());
+  // addInQueue.get("/:command", (req, res) => {
+  //   var ret = {};
+  //   var command = req.params.command;
+  //   ret["command"] = command;
+  //   firebase
+  //     .ref("/queue")
+  //     .push()
+  //     .set(command);
+
+  //   res.status(200).send(JSON.stringify(ret));
+  // });
+
+  // admin.ref("queue/").set({ test: "test"});
 
 module.exports = router;
--- routes/stat.js
+++ routes/stat.js
@@ -6,13 +6,4 @@ router.get('/', function (req, res, next) {
   res.render('stat')
 })
 
-router.post("/data", function(req, res, next) {
-  var rawData = req.body;
-  console.log("TCL: rawData", rawData);
-  return res.redirect("/");
-});
-
-// 1분 동안 들어온 데이터 String으로 저장
-// 1분 간격으로 py에 데이터 전달 (setInterval)
-
 module.exports = router;
