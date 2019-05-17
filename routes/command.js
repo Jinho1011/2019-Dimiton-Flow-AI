10c15eb34eed6ebd76d2f433d6afc2c5bca9fc36
전진호 <gino9940@gmail.com>
2019-05-17 19:55:37 +0900

    data handling

--- routes/command.js
+++ routes/command.js
@@ -3,37 +3,22 @@ var router = express.Router();
 var admin = require("firebase-admin");
 var serviceAccount = require("./automatic-dam-firebase-adminsdk-boz33-fe04b539aa.json");
 
-router.get('/', function (req, res, next) {
-  res.render('command')
-})
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://automatic-dam.firebaseio.com"
+});
 
-router.post('/', function(req, res, next) {
-    console.log("TCL: req", req.body)
-    res.redirect('/cmd')
-})
+var database = admin.database();
+var ref = database.ref("queue/");
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://automatic-dam.firebaseio.com"
-  });
+router.get("/", function(req, res, next) {
+  res.render("command");
+});
 
-  // var database = admin.database();
-  // var ref = database.ref("queue/");
-  // ref.push().set('on1')
-
-  // addInQueue.use(cors());
-  // addInQueue.get("/:command", (req, res) => {
-  //   var ret = {};
-  //   var command = req.params.command;
-  //   ret["command"] = command;
-  //   firebase
-  //     .ref("/queue")
-  //     .push()
-  //     .set(command);
-
-  //   res.status(200).send(JSON.stringify(ret));
-  // });
-
-  // admin.ref("queue/").set({ test: "test"});
+router.post("/", function(req, res, next) {
+  var cmd = req.body.cmd;
+  res.redirect("/cmd");
+  ref.push().set(cmd);
+});
 
 module.exports = router;
--- routes/stat.js
+++ routes/stat.js
@@ -1,5 +1,6 @@
 var express = require("express");
 const { exec } = require("child_process");
+var fs = require('fs');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -11,8 +12,5 @@ router.post("/data", function(req, res, next) {
 	console.log("TCL: cmd", cmd)
   return res.redirect("/");
 });
-
-// 1분 동안 들어온 데이터 String으로 저장
-// 1분 간격으로 py에 데이터 전달 (setInterval)
 
 module.exports = router;
