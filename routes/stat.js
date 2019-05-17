bc531b30654636638eaa560db963cd4be5bd23ef
전진호 <gino9940@gmail.com>
2019-05-17 12:27:21 +0900

    Update tcp_app.js

--- routes/stat.js
+++ routes/stat.js
@@ -6,4 +6,13 @@ router.get('/', function (req, res, next) {
   res.render('stat')
 })
 
+router.post("/data", function(req, res, next) {
+  var {cmd} = req.body;
+	console.log("TCL: cmd", cmd)
+  return res.redirect("/");
+});
+
+// 1분 동안 들어온 데이터 String으로 저장
+// 1분 간격으로 py에 데이터 전달 (setInterval)
+
 module.exports = router;
