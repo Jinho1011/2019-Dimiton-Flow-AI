5fe3c7fa5aa56f238b28cb83a14dbfd74106b989
전진호 <gino9940@gmail.com>
2019-06-17 09:17:02 +0900

    data binding done

--- routes/command.js
+++ routes/command.js
@@ -1,27 +1,28 @@
-var express = require("express");
-var router = express.Router();
-var admin = require("firebase-admin");
-var serviceAccount = require("./automatic-dam-firebase-adminsdk-boz33-fe04b539aa.json");
+var express = require('express')
+var router = express.Router()
+var admin = require('firebase-admin')
+// var serviceAccount = require('./automatic-dam-firebase-adminsdk-boz33-fe04b539aa.json')
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://automatic-dam.firebaseio.com"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://automatic-dam.firebaseio.com'
+// })
 
-var database = admin.database();
-var ref = database.ref("queue/");
+// var database = admin.database()
+// var ref = database.ref('queue/')
 
-router.get("/", function(req, res, next) {
-  res.render("command");
-});
+router.get('/', function (req, res, next) {
+  res.render('command')
+})
 
-router.post("/", function(req, res, next) {
-  console.log("TCL: req", req.body)
-  setTimeout(function() {
-    var cmd = req.body.cmd;
-    res.redirect("/cmd");
-    ref.push().set(cmd);
-  }, 500);
-});
+router.post('/', function (req, res, next) {
+  console.log('TCL: req', req.body)
+  setTimeout(function () {
+    var cmd = req.body.cmd
+    console.log("TCL: cmd", cmd)
+    res.redirect('/cmd')
+    // ref.push().set(cmd)
+  }, 500)
+})
 
-module.exports = router;
+module.exports = router
--- routes/stat.js
+++ routes/stat.js
@@ -1,31 +1,126 @@
 const express = require('express')
 const fs = require('fs')
 const router = express.Router()
+var admin = require('firebase-admin')
+var serviceAccount = require('./automatic-dam-firebase-adminsdk-boz33-fe04b539aa.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://automatic-dam.firebaseio.com'
+})
+
+var database = admin.database()
+var ref = database.ref('queue/')
+
 let WATER_LEVEL
 let TURBIDITY
+let isChangable = true
+let isDischargingNow = false
 
+var app = require('express')()
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+var csvData
+fs.readFile('./tensorflow/easy.csv', { encoding: 'utf-8' }, function (err, data) {
+  if (err) { console.log('TCL: err', err) }
+  csvData = data.split('\n')
+})
+var waterDischarge = []
+var waterLevel = []
+var waterDischargeCnt = 0
+var waterLevelCnt = 0
+
+io.on('connection', (socket) => {
+  io.emit('water-level-2', WATER_LEVEL)
+})
+
+setInterval(() => {
+  io.emit('water-level-2', WATER_LEVEL)
+  io.emit('discharge-status-2', isDischargingNow)
+  io.emit('water-quality-2', TURBIDITY)
+}, 500)
+
+setInterval(() => {
+  for (var i in csvData) {
+    waterDischarge[i] = csvData[i].split(',')[0]
+    waterLevel[i] = csvData[i].split(',')[4]
+  }
+  io.emit('water-discharge-1', waterDischarge[waterDischargeCnt])
+  io.emit('water-discharge-2', Math.abs(waterDischarge[waterDischargeCnt] * (Math.cos(waterDischarge[waterDischargeCnt] ^ 2 + 4 * Math.cos(waterDischarge[waterDischargeCnt] + 100)) / 9)))
+  io.emit('water-level-1', waterLevel[waterLevelCnt])
+  waterDischargeCnt++
+  waterLevelCnt++
+}, 10000)
+
+server.listen(8080, function () {
+  console.log('Socket IO server listening on port 8080')
+})
+
+// UDP CODE
 const dgram = require('dgram')
-const socket = dgram.createSocket('udp4')
-socket.bind(3000)
+const UDPsocket = dgram.createSocket('udp4')
+UDPsocket.bind(3000)
 
-socket.on('listening', function () {
+UDPsocket.on('listening', function () {
   console.log('listening event')
 })
 
-socket.on('message', function (msg, rinfo) {
+UDPsocket.on('message', function (msg, rinfo) {
   const UDP_RES = msg.toString()
-  console.log('TCL: UDP_RES', UDP_RES)
+
   WATER_LEVEL = UDP_RES.split(',')[0]
   TURBIDITY = UDP_RES.split(',')[1]
-  // fs.appendFile("./tensorflow/data-set.csv", WATER_LEVEL + "\n", function(err) {
-  //   if (err) throw err;
-  // });
+
+  if (WATER_LEVEL > 8) {
+    if (isChangable) {
+      ref.push().set('on')
+      isChangable = false
+      isDischargingNow = true
+    }
+  } else {
+    if (!isChangable) {
+      ref.push().set('off1')
+      isChangable = true
+      isDischargingNow = false
+    }
+  }
 })
+// fs.appendFile("./tensorflow/easy.csv", WATER_LEVEL + "\n", function(err) {
+//   if (err) throw err;
+// });
 
 router.get('/', function (req, res, next) {
   res.render('stat')
-  console.log('TCL: WATER_LEVEL', WATER_LEVEL)
-  console.log('TCL: TURBIDITY', TURBIDITY)
+})
+
+router.get('/on1', function (req, res, next) {
+  ref.push().set('on1')
+  res.redirect('/stat')
+})
+
+router.get('/on2', function (req, res, next) {
+  ref.push().set('on2')
+  res.redirect('/stat')
+})
+
+router.get('/off2', function (req, res, next) {
+  ref.push().set('off2')
+  res.redirect('/stat')
+})
+
+router.get('/off1', function (req, res, next) {
+  ref.push().set('off1')
+  res.redirect('/stat')
+})
+
+router.get('/onall', function (req, res, next) {
+  ref.push().set('onall')
+  res.redirect('/stat')
+})
+
+router.get('/offall', function (req, res, next) {
+  ref.push().set('offall')
+  res.redirect('/stat')
 })
 
 module.exports = router
