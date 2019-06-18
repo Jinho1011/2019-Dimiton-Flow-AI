df7ac757f86a5cdf75ef21edd5080af520bee216
전진호 <gino9940@gmail.com>
2019-06-18 13:09:02 +0900

    command

--- routes/command.js
+++ routes/command.js
@@ -19,13 +19,25 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
   switch (req.body.cmd) {
+    case 'reset to 14:32 (872)':
+      timeLeft = 872
+      ref.set(timeLeft)
+      break
     case 'decrease 1 min':
       timeLeft = timeLeft - 60
-      ref.set(timeLeft - 60)
+      ref.set(timeLeft)
       break
     case 'decrease 30 sec':
       timeLeft = timeLeft - 30
-      ref.set(timeLeft - 30)
+      ref.set(timeLeft)
+      break
+    case 'increase 1 min':
+      timeLeft = timeLeft + 60
+      ref.set(timeLeft)
+      break
+    case 'increase 30 sec':
+      timeLeft = timeLeft + 30
+      ref.set(timeLeft)
       break
   }
   res.redirect('/cmd')
